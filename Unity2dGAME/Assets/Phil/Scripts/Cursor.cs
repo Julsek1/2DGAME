1ed09b22ca45c86886c9369f5ccb87bd7fcf2e6e
Philip Frattolillo <phil.fratt@gmail.com>
2020-02-25 11:50:02 -0500

    Level Select
    Added a level select screen with an animated cursor

---
+++ Unity2dGAME/Assets/Phil/Scripts/Cursor.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cursor : MonoBehaviour
+{
+    [SerializeField] Button[] levels;
+    [SerializeField] Sprite[] sprites;
+
+    const float ANIMATION_SPEED = 0.25f;
+    float animationSpeed;
+    int spriteIndex;
+
+    int levelIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = levels[0].transform.position;
+        GetComponent<Image>().sprite = sprites[0];
+        animationSpeed = ANIMATION_SPEED;
+        spriteIndex = 0;
+        levelIndex = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Animate();
+        Movement();
+        Select();
+    }
+
+    private void Select()
+    {
+        if (Input.GetButtonDown("Submit"))
+        {
+            levels[levelIndex].onClick.Invoke();
+        }
+    }
+
+    private void Movement()
+    {
+        if (Input.GetButtonDown("Left"))
+        {
+            if (levelIndex != 0)
+            {
+                transform.position = levels[--levelIndex].transform.position;
+            }
+        }
+
+        else if (Input.GetButtonDown("Right"))
+        {
+            if (levelIndex != levels.Length - 1)
+            {
+                transform.position = levels[++levelIndex].transform.position;
+            }
+        }
+    }
+
+    private void Animate()
+    {
+        animationSpeed -= Time.deltaTime;
+        if (animationSpeed <= 0)
+        {
+            if (spriteIndex == sprites.Length)
+            {
+                spriteIndex = 0;
+            }
+
+            GetComponent<Image>().sprite = sprites[spriteIndex++];
+            animationSpeed = ANIMATION_SPEED;
+        }
+    }
+}
--- Unity2dGAME/Assets/Phil/Scripts/Wind.cs
+++ Unity2dGAME/Assets/Phil/Scripts/Wind.cs
@@ -4,25 +4,67 @@ using UnityEngine;
 
 public class Wind : MonoBehaviour
 {
+    public enum EnumDirection { Left, Right, Up, Down };
+
+    [SerializeField] float speed = 0.01f;
+    [SerializeField] int power = 55;
+    [SerializeField] EnumDirection dir;
+
+    Vector2 windPushDirection;
+    Vector2 windMoveDirection;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        switch (dir)
+        {
+            case EnumDirection.Left:
+                windPushDirection = Vector2.left;
+                break;
+            case EnumDirection.Right:
+                windPushDirection = Vector2.right;
+                break;
+            case EnumDirection.Up:
+                windPushDirection = Vector2.up;
+                break;
+            case EnumDirection.Down:
+                windPushDirection = Vector2.down;
+                break;
+            default:
+                windPushDirection = Vector2.left;
+                break;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector2(transform.position.x - 0.01f, 0f);
+        switch (dir)
+        {
+            case EnumDirection.Left:
+                windMoveDirection = new Vector2(transform.position.x - speed, 0f);
+                break;
+            case EnumDirection.Right:
+                windMoveDirection = new Vector2(transform.position.x + speed, 0f);
+                break;
+            case EnumDirection.Up:
+                windMoveDirection = new Vector2(0f, transform.position.y + speed);
+                break;
+            case EnumDirection.Down:
+                windMoveDirection = new Vector2(0f, transform.position.y - speed);
+                break;
+            default:
+                windMoveDirection = new Vector2(transform.position.x - speed, 0f);
+                break;
+        }
+        transform.position = windMoveDirection;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.name == "Player" && collision.GetType() == typeof(BoxCollider2D))
         {
-            //take velocity absolute and add it to the upward force
-            collision.GetComponent<Movement>().RestoreJumps();
-            collision.GetComponent<Rigidbody2D>().AddForce(Vector2.left * 50);
+            collision.GetComponent<Rigidbody2D>().AddForce(windPushDirection * power);
         }
     }
 }
