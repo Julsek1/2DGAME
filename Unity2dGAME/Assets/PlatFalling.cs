3c4be17378f4636a614f504da0136ec1ce59551c
Julian Grajales <juliangrajales@gmail.com>
2020-02-27 19:54:07 -0500

    Falling Platform
    Script + sprite

---
+++ Unity2dGAME/Assets/PlatFalling.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatFalling : MonoBehaviour
+{
+
+    Rigidbody2D rigBod; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigBod = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name.Equals("Player")){
+            Invoke("DropPlatform", 0.5f);
+            Destroy(gameObject, 2f);
+                
+        }
+    }
+
+    void DropPlatform()
+    {
+        rigBod.isKinematic = false;
+    }
+}
