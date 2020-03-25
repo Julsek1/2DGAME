41bc49fde82d144c63326e437c9721d0ea4d53c9
Julian Grajales <juliangrajales@gmail.com>
2020-03-25 19:01:41 -0400

    Spike Moving Trap
    sprites and set up

---
+++ Unity2dGAME/Assets/PressTrap.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressTrap : MonoBehaviour
+{
+    private Vector3 posA;
+    private Vector3 posB;
+
+    private Vector3 nextPos;
+
+    [SerializeField]
+    private float speed;
+
+
+    [SerializeField]
+    private Transform childTransform;
+
+    [SerializeField]
+    private Transform transformB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        posA = childTransform.localPosition;
+        posB = transformB.localPosition;
+        nextPos = posB;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nextPos, speed * Time.deltaTime);
+        if (Vector3.Distance(childTransform.localPosition, nextPos) <= 0.1)
+        {
+           
+            NextDestination();
+            
+        }
+    }
+
+    private void NextDestination()
+    {
+        
+        nextPos = nextPos != posA ? posA : posB;
+    }
+}
