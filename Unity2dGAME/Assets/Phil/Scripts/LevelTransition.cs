b29ac479941ed817b9e6033e6af5cab89457f0a3
Philip Frattolillo <phil.fratt@gmail.com>
2020-04-03 15:04:19 -0400

    Level transition change
    Changed the way that the level transition happens. (starts small and expands out, revealing more of the level)

--- Unity2dGAME/Assets/Phil/Scripts/LevelTransition.cs
+++ Unity2dGAME/Assets/Phil/Scripts/LevelTransition.cs
@@ -18,8 +18,6 @@ public class LevelTransition : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //lifetime -= Time.deltaTime;
-
         if (countDown)
         {
             lifetime -= Time.unscaledDeltaTime;
@@ -34,8 +32,27 @@ public class LevelTransition : MonoBehaviour
 
             else
             {
-                panel.gameObject.GetComponent<Image>().color = new Color(0, 0, 0, lifetime);
+                //panel.gameObject.transform.localScale = new Vector3(lifetime * 4, lifetime * 4);//shrink in
+                panel.gameObject.transform.localScale = new Vector3(-120 * lifetime + 122, -120 * lifetime + 122);
             }
         }
+
+        //if (countDown)
+        //{
+        //    lifetime -= Time.unscaledDeltaTime;
+
+        //    if (lifetime <= 0)
+        //    {
+        //        countDown = false;
+        //        panel.SetActive(false);
+        //        player.EnableControls();
+
+        //    }
+
+        //    else
+        //    {
+        //        panel.gameObject.GetComponent<Image>().color = new Color(0, 0, 0, lifetime);
+        //    }
+        //}
     }
 }
