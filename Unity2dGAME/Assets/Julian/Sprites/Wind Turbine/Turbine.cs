37f8b2e1efe1321d7e2510fc2260d94628a00c9a
Julian Grajales <juliangrajales@gmail.com>
2020-04-04 00:24:03 -0400

    working with wind

---
+++ Unity2dGAME/Assets/Julian/Sprites/Wind Turbine/Turbine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turbine : MonoBehaviour
+{
+
+    public float rotationSpeed;
+
+    void Start()
+    {
+        
+    }
+
+   
+    void FixedUpdate()
+    {
+        this.transform.Rotate(new Vector3(0, 0, rotationSpeed)); 
+    }
+}
