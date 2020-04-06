e02354f900414ec229b8fda4488ace8a58dbdba8
Julian Grajales <juliangrajales@gmail.com>
2020-04-05 23:15:42 -0400

    Pendulum Trap
    Script
    Sprites
    Hinge movement

---
+++ Unity2dGAME/Assets/Pendulum.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pendulum : MonoBehaviour
+{
+    
+    public Rigidbody2D rigidB2d;
+    public float leftPushRange;
+    public float rightPushRange;
+    public float velocityThreshold;
+
+
+    void Start()
+    {
+        rigidB2d = GetComponent<Rigidbody2D>();
+        rigidB2d.angularVelocity = velocityThreshold;
+    }
+
+    
+    void Update()
+    {
+
+        Push(); 
+    }
+
+    public void Push()
+    {
+        if(transform.rotation.z > 0
+            && transform.rotation.z < rightPushRange
+            && (rigidB2d.angularVelocity > 0)
+            && rigidB2d.angularVelocity < velocityThreshold)
+         
+        {
+            rigidB2d.angularVelocity = velocityThreshold;
+
+        }
+        else if(transform.rotation.z < 0
+            && transform.rotation.z > leftPushRange
+            && (rigidB2d.angularVelocity < 0)
+            && rigidB2d.angularVelocity > velocityThreshold* -1)
+        {
+            rigidB2d.angularVelocity = velocityThreshold* -1;
+
+        }
+    }
+}
