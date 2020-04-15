d9ce5624b459c84f05ff842946e79e8bbe8350c0
Philip Frattolillo <phil.fratt@gmail.com>
2020-04-15 16:22:38 -0400

    Pause menu/victory level
    Added and configured a pause menu
    Added a victory level

---
+++ Unity2dGAME/Assets/Phil/Scripts/PauseMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] Button resume;
+
+    private void OnEnable()
+    {
+        SetSelectedButton();
+    }
+
+    private void SetSelectedButton()
+    {
+        resume.Select();
+        resume.OnSelect(null);
+    }
+}
